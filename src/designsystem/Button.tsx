2d4c816d5b582aeb8c9cd249e1c7a6530d4f8d5e
aleeeeza <108175707+aleeeeza@users.noreply.github.com>
2024-02-01 15:05:19 +0800

    feat: implement Button component (#1)
    * chore: update versions of react typings to fix TS7026 error
    
    * feat: implement button component
    
    * refactor: pin versions, remove unnecessary props, update component code to use clsx utility
    
    ---------
    
    Co-authored-by: frances aleeza <francesaleeza@francess-MacBook-Air.local>

--- src/designsystem/Button.tsx
+++ src/designsystem/Button.tsx
@@ -1,52 +1,34 @@
+import clsx from 'clsx';
 import React from 'react';
-import './button.css';
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  color?: 'primary' | 'secondary';
+  size?: 'medium' | 'large';
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
+  color = 'primary',
+  size = 'large',
+  children,
+  ...rest
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const buttonClass = clsx(
+    'w-full font-bold rounded-full disabled:opacity-50',
+    {
+      'bg-primary-btn text-white enabled:hover:bg-primary-btn-hover': color === 'primary',
+      'bg-secondary-btn text-secondary-btn-label enabled:hover:bg-secondary-btn-hover': color === 'secondary',
+      'py-2.5 px-4 text-base leading-5': size === 'medium',
+      'py-4 px-7 text-lg leading-6': size === 'large',
+    }
+  );
+
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      {...props}
+      className={buttonClass}
+      {...rest} 
     >
-      {label}
-      <style jsx>{`
-        button {
-          background-color: ${backgroundColor};
-        }
-      `}</style>
+      {children}
     </button>
   );
 };
