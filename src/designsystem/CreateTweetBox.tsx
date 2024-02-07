acba88bd3956aa96fa7068f854e929c008361e2d
frances aleeza <francesaleeza@francess-MacBook-Air.local>
2024-02-07 14:17:55 +0800

    refactor: clean up code, update event type, remove clsx, add newline to end of file

--- src/designsystem/CreateTweetBox.tsx
+++ src/designsystem/CreateTweetBox.tsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Button } from "./Button";
-import clsx from "clsx";
 import { AvatarLink, AvatarLinkProps } from "./AvatarLink";
 
 interface CreateTweetBoxProps extends AvatarLinkProps {
@@ -17,51 +16,25 @@ export const CreateTweetBox = ({
 }: CreateTweetBoxProps) => {
     const [tweet, setTweet] = useState('');
 
-    const handleTextChange = (e: any) => {
-        console.log('changed!', tweet);
+    const handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
         onTextChange(tweet);
         setTweet(e.target.value);
     };
 
-    const handleSubmit = (e: any) => {
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('submit!', tweet);
         onSubmit(tweet);
         setTweet('');
     };
 
-    const createTweetBoxClass = clsx(
-        "w-full flex flex-row px-4 py-1"
-    );
-
-    const avatarClass = clsx(
-        "mr-3.5"
-    );
-
-    const textAreaClass = clsx(
-        "bg-transparent text-[#d9d9d9] text-xl min-w-80 w-96 min-h-14"
-    );
-
-    const buttonsRowClass = clsx(
-        "flex flex-row"
-    );
-
-    const mediaButtonsClass = clsx(
-        "basis-3/4"
-    );
-
-    const tweetButtonClass = clsx(
-        "basis-1/4"
-    );
-
     return (
-        <div className={createTweetBoxClass}>
-            <div className={avatarClass}>
+        <div className="w-full flex flex-row px-4 py-1">
+            <div className="mr-3.5">
                 <AvatarLink href={href} imgSrc={imgSrc} alt={alt} size="small" />
             </div>
             <form onSubmit={handleSubmit}>
                 <textarea 
-                    className={textAreaClass}
+                    className="bg-transparent text-[#d9d9d9] text-xl min-w-80 w-96 min-h-14"
                     rows={2}
                     cols={33}
                     placeholder="What's happening?"
@@ -69,10 +42,14 @@ export const CreateTweetBox = ({
                     onChange={handleTextChange}
                     maxLength={218}
                 />
-                <div className={buttonsRowClass}>
-                    <div className={mediaButtonsClass}>Test Other Buttons</div>
-                    <div className={tweetButtonClass}>
-                        <Button type="submit" size="medium" disabled={tweet.length === 0}>Tweet</Button>
+                <div className="flex flex-row">
+                    <div className="basis-3/4">
+                        Other Media Buttons Placeholder
+                    </div>
+                    <div className="basis-1/4">
+                        <Button type="submit" size="medium" disabled={tweet.length === 0}>
+                            Tweet
+                        </Button>
                     </div>
                 </div>
             </form>
